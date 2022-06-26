7227e0706f6be6797610f3d22f23bbf596cecaab
Adam Davis <adavisdeveloper@gmail.com>
2022-06-26 15:54:16 -0400

    add more entries

--- entries.js
+++ entries.js
@@ -69,4 +69,102 @@ export default [
                 You had a smile that would light up a room.
                 `
     },
+    {
+        title: "Looking for Keyanna (or Kiana, Keyana, Kieanna)",
+        content: `
+                I'm looking for someone named Keyanna (or maybe it's spelled Kianna?) 
+                who was a really cool alternative black girl with colored hair. 
+                I gave you a ride once in my truck and then we texted back and forth for awhile. 
+                I miss our conversations and wish we could connect again. 
+                Hope you see this.
+                `
+    },
+    {
+        title: "Down the road",
+        content: `
+                I thought you looked familiar the way we were both making eye contact at the store. 
+                Sometimes you can't deny the chemistry.
+                `
+    },
+    {
+        title: "Planet Fitness",
+        content: `
+                We talked about Rocky Face Park.
+                I didn't even get your name.  Want to go hiking but like to have a buddy along.
+                `
+    },
+    {
+        title: "Left without saying bye",
+        content: `
+                You clean our office and left without saying goodbye…reach out if you'd like to chat a lil bit
+                `
+    },
+    {
+        title: "we use to walk and talk",
+        content: `
+                If you happen to see this we use to walk our dog's around the same time and you haven't been around for sometime now and I just wanted to make sure all is good, 
+                hoping we can walk and talk again because I use to look forward to the conversations we had and clearing my mind of everyday things.
+                `
+    },
+    {
+        title: "My keys??",
+        content: `
+                I pet your dog at the pilot on lovell sweet dog but I may have dropped my keys in your car.…
+                `
+    },
+    {
+        title: "We all talked at the park",
+        content: `
+                We all happened to talk at the lake in rural retreat VA seem to hit it off a bit we are new here an would love to see if we could find you again or other couples to hang an talk with
+                `
+    },
+    {
+        title: "cat and mouse",
+        content: `
+                We played cat and mouse till i had to take the ga road. Would like to 
+                get up with you. you were in a blue commercial vehicle , I was in a dark
+                gray sport sedan.
+
+                Amazing smile dude!!
+                `
+    },
+    {
+        title: "Felicity",
+        content: `
+                Our kids played together today at the rec park and later at the farmer's market and aquarium. 
+                You mentioned story time at the library as we were leaving. I should've gotten your number. Let's connect!
+                `
+    },
+    {
+        title: "Guy named T.V.",
+        content: `
+                I am searching for a guy with the initials T.V who was at @@@@ Hospital in Louisville, TN on December 17, 2021. He gave me his phone number but they took it away from me because you cannot share personal information. He looked like Harry Potter or some would say Johnny Depp and had neck tattoos. He was previously incarcerated. Claimed to live off @@@@ St.
+                `
+    },
+    {
+        title: "Hibiscus Sour",
+        content: `
+                You came into the bar I work at and ordered a hibiscus sour. We talked for a bit and I was too scared to ask for your number.
+                `
+    },
+    {
+        title: "Ollie's shopper",
+        content: `
+                Shopping at Ollie's my cart was full and was ready to check out and head home, but decided i better use the restroom first,not being much room in there i left my buggy out side, when i finished i came out to a cart that hadn't been bothered and another shopper said that the had keep watch of it for me, how nice was that.... Thank You !!!!
+                `
+    },
+    {
+        title: "Flip Flops",
+        content: `
+                At the garage sale today.
+
+                We were both looking over the books for sale.
+
+                I mentioned your flip flops looked great with that fresh pedi.
+
+                Wish I could have spoken to you longer.
+
+                What book did I buy?
+                `
+    },
 ]
