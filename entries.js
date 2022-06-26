23a0d90657e616c3128aef87a7c142f1a09f54ce
Adam Davis <adavisdeveloper@gmail.com>
2022-06-26 14:23:31 -0400

    added more entries

--- entries.js
+++ entries.js
@@ -18,4 +18,55 @@ export default [
                 Said you worked at tires plus.
                 `
     },
+    {
+        title: "I missed my old friend",
+        content: `
+                I came by your place and we had good times 
+                lost touch with you love to get back together 
+                I drove the gray truck
+                `
+    },
+    {
+        title: "Fishing on River Rd under Bridge",
+        content: `
+                We were both fishing under the 41 bridge in Cartersville last weekend. 
+                We talked about the best bait and you let me have some of yours. 
+                I was feeling a connection but was nervous. 
+                You were in red pickup and had on Nike jogging shorts and a blue tank top. 
+                I hope you see this. I was the guy with the blue ford.
+                `
+    },
+    {
+        title: "Large grandmother driving on 400",
+        content: `
+                You looked at me as I was driving next to you on hwy and I thought you winked at me. 
+                I'm hoping I was right.
+                `
+    },
+    {
+        title: "BP gas station",
+        content: `
+                Kept making eye contact!! I was in lol blue car, you were in grey car with two other people
+                Lunch or dinner sounds great?
+                `
+    },
+    {
+        title: "PUBLIX & Gelato",
+        content: `
+                You were in front of me at the PUBLIX in @@@@@. We chatted about 
+                the gelato and the flowers...then you waved at me from your car.  Let's 
+                connect.
+
+                I know this is a long shot, but...
+                `
+    },
+    {
+        title: "Inviting Smile",
+        content: `
+                We exchanged smiles at the Farmers Market in Duluth this morning! You 
+                were around 5'0 -5'3” black hair and beautiful porcelain skin!
+
+                You had a smile that would light up a room.
+                `
+    },
 ]
