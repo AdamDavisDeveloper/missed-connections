d76f44ef4bff4bb0033174d72c74617829382e11
Adam Davis <adavisdeveloper@gmail.com>
2022-06-26 15:54:41 -0400

    create a past index checker to avoid repeating same entry

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const EntryTitle = document.getElementById('EntryTitle');
 const EntryContent = document.getElementById('EntryContent');
 const Refresh = document.getElementById("Refresh");
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+/* 
+    the index of the entry you're currently on / are about to switch from.
+    used to check if the new random index is the same as the previous to
+    prevent duplicate entries on Refresh 
+*/
+let thisIndex;
+
+function getRandomInt() {
+    let randomInt = Math.floor(Math.random() * (entries.length - 1) + 1);
+    return (randomInt === thisIndex) ? getRandomInt() : randomInt;
 }
 
 function newEntry() {
-    const entryIndex = getRandomInt(0, entries.length - 1);
-    console.log(entryIndex);
-    EntryTitle.innerText = entries[entryIndex].title;
-    EntryContent.innerText = entries[entryIndex].content;
+    thisIndex = getRandomInt();
+    EntryTitle.innerText = entries[thisIndex].title;
+    EntryContent.innerText = entries[thisIndex].content;
 }
 
 Refresh.addEventListener("click", newEntry);
